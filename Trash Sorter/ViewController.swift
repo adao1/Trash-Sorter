d76b959bbf25a1faae57e41116ce018d35c57aff
Alexander Dao <adao@berkeley.edu>
2016-06-24 13:29:39 -0700

    finished v1.0

--- Trash Sorter/ViewController.swift
+++ Trash Sorter/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var trashNameTextField: UITextField!
+    @IBOutlet weak var trashTypeSegmentedControl: UISegmentedControl!
+    
+    var composte = [String]()
+    var recyclable = [String]()
+    var trash = [String]()
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onThrowAwayBtnClicked(sender: UIButton) {
+        print("test")
+        
+        if let trashName = trashNameTextField.text {
+            switch trashTypeSegmentedControl.selectedSegmentIndex {
+            case 0:
+                composte.append(trashName)
+            case 1:
+                recyclable.append(trashName)
+            case 2:
+                trash.append(trashName)
+            default:
+                break
+            }
+            
+            trashNameTextField.text = ""
+            print(composte)
+            print(recyclable)
+            print(trash)
+        }
     }
-
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let threeTypesVC = segue.destinationViewController as! ThreeTypesVC
+        threeTypesVC.composte1 = composte
+        threeTypesVC.recyclable1 = recyclable
+        threeTypesVC.trash1 = trash
+    }
 }
 
